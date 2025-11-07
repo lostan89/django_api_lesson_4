9eb7eb2981c6f799c3cdb4d8945e08b001547fed
lostan89 <skeeperenator@gmail.com>
2025-11-07 18:31:37 +0300

    Update get_nasa_image.py

--- get_nasa_image.py
+++ get_nasa_image.py
@@ -3,25 +3,25 @@ from environs import Env
 from additional import save_image_to_path
 
 
-def get_nasa_image(api_key, count):
+def get_nasa_images(api_key, count):
     api_url = "https://api.nasa.gov/planetary/apod"
     payload = {"api_key": api_key, "count": count}
     response = requests.get(api_url, params=payload)
     response.raise_for_status()
-    nasa_photo = response.json()
-    image_url = []
-    for image in nasa_photo:
+    nasa_photos = response.json()
+    image_urls = []
+    for image in nasa_photos:
         url = image.get("url")
         if url:
-            image_url.append(url)
-    return image_url
+            image_urls.append(url)
+    return image_urls
 
 
 def main():
     env = Env()
     env.read_env()
     api_key = env.str("NASA_API_KEY")
-    nasa_image = get_nasa_image(api_key, 30)
+    nasa_images = get_nasa_images(api_key, 30)
     for image_number, image_url in enumerate(nasa_image):
         save_image_to_path(f"nasa_apod_{str(image_number)}", image_url, "images/")
 
